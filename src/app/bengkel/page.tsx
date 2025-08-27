5979e9101e9b2041193536f480468ce0761cf3b1
sadamlrfs <sadamalirafsanjani365@gmail.com>
2025-08-27 20:15:05 +0700

    fix: fix link

--- src/app/bengkel/page.tsx
+++ src/app/bengkel/page.tsx
@@ -28,9 +28,10 @@ export default function BengkelPage() {
           </div>
 
           <div className="flex justify-center flex-col mt-6 w-fit ">
-            <Button 
+            <Button
               onClick={() => {
-                window.location.href = "https://linktr.ee/BSO_BENGKEL_HMDMITS?fbclid=PAZXh0bgNhZW0CMTEAAaf7-SnyExActRG1JLes26GLgbJMVkclJ_4fcsK9LHNLfIMSLUAzrsxibL2wVg_aem_1OWAVQR29J34_Nhx6L3LOw";
+                window.location.href =
+                  "https://linktr.ee/BSO_BENGKEL_HMDMITS?fbclid=PAZXh0bgNhZW0CMTEAAaf7-SnyExActRG1JLes26GLgbJMVkclJ_4fcsK9LHNLfIMSLUAzrsxibL2wVg_aem_1OWAVQR29J34_Nhx6L3LOw";
               }}
               className="bg-white border-0 hover:border-0 text-black mt-6 py-4 px-20 max-lg:px-5 font-bold text-2xl max-lg:text-base hover:bg-red-700 hover:text-white "
             >
@@ -46,12 +47,13 @@ export default function BengkelPage() {
             </Button>
             <hr className="my-6 border-t-2 border-white opacity-50 w-full" />
             <div className="flex gap-4 w-full items-center justify-center max-lg:flex-col">
-              <Button 
-                 onClick={() => {
-                window.location.href = "https://www.instagram.com/bengkeldemits/";
-              }}
+              <Button
+                onClick={() => {
+                  window.location.href =
+                    "https://www.instagram.com/bengkeldemits/";
+                }}
                 className="border-white bg-transparent text-white  py-3 px-10 max-lg:px-5 gap-2 justify-center items-center font-semibold text-2xl max-lg:text-base hover:bg-red-700 hover:text-white mx-auto"
-                >
+              >
                 <NextImage
                   alt="bengkeldemits-logo"
                   width={24}
@@ -61,12 +63,13 @@ export default function BengkelPage() {
                 />
                 @bengkeldemits
               </Button>
-              <Button 
+              <Button
                 onClick={() => {
-                window.location.href = "https://www.tiktok.com/@bengkeldemits";
-              }}
+                  window.location.href =
+                    "https://www.tiktok.com/@bengkeldemits";
+                }}
                 className="border-white bg-transparent text-white  py-3 px-10 max-lg:px-5 gap-2 justify-center items-center font-semibold text-2xl max-lg:text-base hover:bg-red-700 hover:text-white mx-auto"
-                >
+              >
                 <NextImage
                   alt="bengkeldemits-logo"
                   width={24}
@@ -76,11 +79,12 @@ export default function BengkelPage() {
                 />
                 @bengkeldemits
               </Button>
-              <Button 
-                 onClick={() => {
-                window.location.href = "https://wa.me/6285194924517";
+              <Button
+                onClick={() => {
+                  window.location.href = "https://wa.me/6285194924517";
                 }}
-                className="border-white bg-transparent text-white  py-3 px-10 max-lg:px-5 gap-2 justify-center items-center font-semibold text-2xl max-lg:text-base hover:bg-red-700 hover:text-white mx-auto">
+                className="border-white bg-transparent text-white  py-3 px-10 max-lg:px-5 gap-2 justify-center items-center font-semibold text-2xl max-lg:text-base hover:bg-red-700 hover:text-white mx-auto"
+              >
                 <NextImage
                   alt="bengkeldemits-logo"
                   width={24}
